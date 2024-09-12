e47be9f49b1185bb0a9f81b29efb93131efc1d88
Sofia Bargues <barguessofia@gmail.com>
2024-09-12 09:16:44 +0100

    logic

---
+++ src/components/Moles.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+const allMoles = ["mole1", "mole2", "mole3", "mole4", "mole5", "mole6"];
+const getRandomElement = (elements: string[]) => {
+  const index = Math.floor(Math.random() * elements.length);
+  return elements[index];
+};
+
+export const Moles = () => {
+  const [mole, setMole] = useState(" ");
+  //   const randomMoleIndex = getRandomMole(allMoles);
+  const filterMoles = allMoles.filter((word) => word != mole);
+
+  useEffect(() => {
+    const myTimeOut = setTimeout(() => {
+      setMole(getRandomElement(filterMoles));
+    }, 2000);
+    return () => clearTimeout(myTimeOut);
+  }, [mole]);
+
+  return (
+    <div>
+      <div>{mole}</div>
+      <div>{filterMoles}</div>;
+    </div>
+  );
+};
+
+//create moles
+//create random moles
+//apear random moles
+//apear in short duration
+//desapear the moles
