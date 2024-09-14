2c30aae67db0fddbaedc4ada19f4f249c265d34a
Sofia Bargues <barguessofia@gmail.com>
2024-09-14 15:41:41 +0100

    its working

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,65 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import { Moles } from "./components/Moles";
+// import { Moles } from "./components/Moles";
+
+const allHoles = [
+  "hole1",
+  "hole2",
+  "hole3",
+  "hole4",
+  "hole5",
+  "hole6",
+  "hole7",
+  "hole8",
+  "hole9",
+];
+const getRandomElement = (elements: string[]) => {
+  const index = Math.floor(Math.random() * elements.length);
+  return elements[index];
+};
+
+const Moles = () => {
+  const [mole, setMole] = useState(" ");
+  const [total, setTotal] = useState(0);
+  const [clckedMole, setClckedMole] = useState("");
+
+  useEffect(() => {
+    const filterHoles = allHoles.filter((word) => word != mole);
+    const myTimeOut = setTimeout(() => {
+      setMole(getRandomElement(filterHoles));
+    }, 2000);
+    return () => clearTimeout(myTimeOut);
+  }, [mole]);
+  return (
+    <div
+      className="flex flex-col items-center  bg-[#bef05d]
+     h-screen"
+    >
+      <div className="text-3xl  m-6 ">Total {total}</div>
+      <div className="grid grid-cols-3 max-w-[750px] m-auto items-center ">
+        {allHoles.map((x) => (
+          <div>
+            <button
+              onClick={() => {
+                if (mole != clckedMole) {
+                  setTotal(total + 1);
+                  setClckedMole(mole);
+                }
+              }}
+              disabled={mole != x}
+            >
+              {mole === x ? (
+                <img className="" src="/mole.png" alt="" />
+              ) : (
+                <img src="/hole.png" alt="" />
+              )}
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 function App() {
   return (
--- src/components/Moles.tsx
+++ src/components/Moles.tsx
@@ -1,37 +1,40 @@
 import { useEffect, useState } from "react";
 
-const allMoles = ["mole1", "mole2", "mole3", "mole4", "mole5", "mole6"];
-const getRandomElement = (elements: string[]) => {
-  const index = Math.floor(Math.random() * elements.length);
-  return elements[index];
-};
+// const allMoles = ["mole1", "mole2", "mole3", "mole4", "mole5", "mole6"];
+// const getRandomElement = (elements: string[]) => {
+//   const index = Math.floor(Math.random() * elements.length);
+//   return elements[index];
+// };
 
-export const Moles = () => {
-  const [mole, setMole] = useState(" ");
-  const [holes, setHoles] = useState([""]);
-  //   const randomMoleIndex = getRandomMole(allMoles);
+// export const Moles = () => {
+//   const [mole, setMole] = useState(" ");
+//   const [holes, setHoles] = useState([""]);
+//   //   const randomMoleIndex = getRandomMole(allMoles);
 
-  useEffect(() => {
-    const filterMoles = allMoles.filter((word) => word != mole);
-    const myTimeOut = setTimeout(() => {
-      const holes = [...filterMoles];
-      setHoles(holes);
-      setMole(getRandomElement(filterMoles));
-    }, 2000);
-    return () => clearTimeout(myTimeOut);
-  }, [mole]);
+//   useEffect(() => {
+//     const filterMoles = allMoles.filter((word) => word != mole);
+//     const myTimeOut = setTimeout(() => {
+//       const holes = [...filterMoles];
+//       setHoles(holes);
+//       setMole(getRandomElement(filterMoles));
+//     }, 2000);
+//     return () => clearTimeout(myTimeOut);
+//   }, [mole]);
 
-  return (
-    <div>
-      <div className="grid grid-cols-">
-        <div className="h-24 w-24 border-2">{mole}</div>
-        {holes.map((x) => (
-          <div className="h-24 w-24 border-2">{x}</div>
-        ))}
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <div className="grid grid-cols-">
+//         <div className="h-24 w-24 border-2">
+
+//     <img ></img>
+//           {mole}</div>
+//         {holes.map((x) => (
+//           <div className="h-24 w-24 border-2">{x}</div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
 
 //create moles
 //create random moles
